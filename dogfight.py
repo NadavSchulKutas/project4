47c956879da157b0fc6796f5846cebdc309f9cb6
NadavSchulKutas <94572293+NadavSchulKutas@users.noreply.github.com>
2021-11-28 11:19:26 -0800

    Added hitboxes, a delay between shots, and i-frames

--- dogfight.py
+++ dogfight.py
@@ -47,6 +47,8 @@ class Shootable(MovingBody):
 
     def is_hit_by(self, photon):
         '''is the vector between the photon and the asteroid center smaller than the asteroid radius? Basically, is the photon inside the asteroid?'''
+        if self.shotTimer > (self.shootDelay - self.iFrames): #Never get hit if you have i-frames
+            return False
         return ((self.position - photon.position).magnitude() < self.radius)
 
     def explode(self):
@@ -59,11 +61,219 @@ class Shootable(MovingBody):
             self.SHRAPNEL_CLASS(self.position,self.world)
         self.leave()
 
+class Ember(MovingBody):
+    '''Little sparks that come off when an asteroid is destroyed'''
+    INITIAL_SPEED = 2.0
+    SLOWDOWN      = 0.2
+    TOO_SLOW      = INITIAL_SPEED / 20.0
+
+    def __init__(self, position0, world):
+        velocity0 = Vector2D.random() * self.INITIAL_SPEED
+        MovingBody.__init__(self, position0, velocity0, world)
+
+    def color(self):
+        white_hot  = "#FFFFFF"
+        burning    = "#FF8080"
+        smoldering = "#808040"
+        speed = self.velocity.magnitude()
+        if speed / self.INITIAL_SPEED > 0.5:
+            return white_hot
+        if speed / self.INITIAL_SPEED > 0.25:
+            return burning
+        return smoldering
+
+    def steer(self):
+        return -self.velocity.direction() * self.SLOWDOWN
+
+    def update(self):
+        MovingBody.update(self)
+        if self.velocity.magnitude() < self.TOO_SLOW:
+            self.leave()
+
+class Photon(MovingBody):
+    '''Projectiles that the player shoots out'''
+    INITIAL_SPEED = 1.2 #Originally "2.0 * SmallAsteroid.MAX_SPEED", I removed the dependence on the asteroid classes, which we're going to remove later
+    LIFETIME      = 70 #Measured in tics, not distance travelled
+
+    def __init__(self,source,world):
+        self.age  = 0
+        v0 = source.velocity + (source.get_heading() * self.INITIAL_SPEED) #Photons currently inherit the player's momentum. When testing the game, we should try having a version where photons don't do this
+        MovingBody.__init__(self, source.position, v0, world)
+
+    def color(self):
+        return "#8080FF"
+
+    def update(self):
+        MovingBody.update(self)
+        self.age += 1
+        if self.age >= self.LIFETIME:
+            self.leave()
+        else:
+            targets = [a for a in self.world.agents if isinstance(a,Shootable)]
+            for t in targets:
+                if t.is_hit_by(self):
+                    t.explode()
+                    self.leave()
+                    return
+
+class Ship(Shootable):
+    SHRAPNEL_CLASS  = Ember
+    SHRAPNEL_PIECES = 4
+    WORTH           = 1
+
+    TURNS_IN_360   = 12
+    IMPULSE_FRAMES = 4
+    ACCELERATION   = 0.05
+    MAX_SPEED      = 2.0
+
+    START_X   = 5
+    START_Y   = 5
+
+    iFrames = 3 #Number of i-frames given to player after a shot. Meant to be used with something like "if self.shotTimer > (self.shootDelay - self.iFrames):"
+    shootDelay = 20 #Delay between shots
+
+    def __init__(self, world, player_one):
+        self.player_one = player_one
+        self.shotTimer = 0 #Players can shoot immediately after spawning
+        xoffset = -self.START_X if player_one else  self.START_X
+        yoffset =  self.START_Y if player_one else -self.START_Y
+        position0    = Point2D(xoffset, yoffset)
+        velocity0    = Vector2D(0.0,0.0)
+        self.speed   = 0.0
+        self.angle   = 90.0
+        self.impulse = 0
+        radius = self.get_heading().magnitude()
+        Shootable.__init__(self, position0, velocity0, radius, world)
+
+
+    def color(self):
+        if self.player_one: #Player one is red
+            if self.shotTimer > (self.shootDelay - self.iFrames):
+                return "#ffaaa1" #Players are a lighter color when invincible
+            return "#f74830"
+        else: #Player two is blue
+            if self.shotTimer > (self.shootDelay - self.iFrames):
+                return "#bcdcff"
+            return "#3090f7"
+
+    def get_heading(self):
+        angle = self.angle * math.pi / 180.0
+        return Vector2D(math.cos(angle), math.sin(angle))
+
+    def turn_left(self):
+        self.angle += 360.0 / self.TURNS_IN_360
+
+    def turn_right(self):
+        self.angle -= 360.0 / self.TURNS_IN_360
+
+    def speed_up(self):
+        self.impulse = self.IMPULSE_FRAMES
+
+    def shoot(self):
+        '''If need be, we can add self.player_one to the variables given to photon and then add code to it so that photons don't hit the player that created them '''
+        Photon(self, self.world)
+        self.shotTimer = self.shootDelay
+
+    def update(self):
+        if self.shotTimer > 0:
+            self.shotTimer -= 1
+        super().update()
+
+    def shape(self):
+        h  = self.get_heading()
+        hp = h.perp()
+        p1 = self.position + h * 1.5 #making ships a little longer
+        p2 = self.position + hp * 0.5
+        p3 = self.position - hp * 0.5
+        return [p1,p2,p3]
+
+    def steer(self):
+        if self.impulse > 0:
+            self.impulse -= 1
+            return self.get_heading() * self.ACCELERATION
+        else:
+            return Vector2D(0.0,0.0)
+
+    def trim_physics(self):
+        MovingBody.trim_physics(self)
+        m = self.velocity.magnitude()
+        if m > self.MAX_SPEED:
+            self.velocity = self.velocity * (self.MAX_SPEED / m)
+            self.impulse = 0
+
+class PlayAsteroids(Game):
+
+    DELAY_START      = 150
+    MAX_ASTEROIDS    = 6
+    INTRODUCE_CHANCE = 0.01
+
+    def __init__(self):
+        Game.__init__(self,"ASTEROIDS!!!",60.0,45.0,800,600,topology='wrapped')
+
+        self.number_of_asteroids = 0
+        self.number_of_shrapnel = 0
+        self.level = 1
+        self.score = 0
+
+        self.before_start_ticks = self.DELAY_START
+        self.started = False
+
+        self.ship_one = Ship(self, player_one=True)
+        self.ship_two = Ship(self, player_one=False)
+
+    def max_asteroids(self):
+        return min(2 + self.level,self.MAX_ASTEROIDS)
+
+    def handle_keypress(self,event):
+        Game.handle_keypress(self,event)
+
+        '''Player One controls: wasd + c to shoot '''
+        if event.char == 'w':
+            self.ship_one.speed_up()
+        if event.char == 'a':
+            self.ship_one.turn_left()
+        elif event.char == 'd':
+            self.ship_one.turn_right()
+        if event.char == 'c' and self.ship_one.shotTimer == 0:
+            self.ship_one.shoot()
+
+        '''Player Two controls: pl;' + , to shoot '''
+        if event.char == 'p':
+            self.ship_two.speed_up()
+        if event.char == 'l':
+            self.ship_two.turn_left()
+        elif event.char == '\'':
+            self.ship_two.turn_right()
+        if event.char == ',' and self.ship_two.shotTimer == 0:
+            self.ship_two.shoot()
+
+    def update(self):
+        # Are we waiting to toss asteroids out?
+        if self.before_start_ticks > 0:
+            self.before_start_ticks -= 1
+        else:
+            self.started = True
+
+        # Should we toss a new asteroid out?
+        if self.started:
+            tense = (self.number_of_asteroids >= self.max_asteroids())
+            tense = tense or (self.number_of_shrapnel >= 2*self.level)
+            if not tense and random.random() < self.INTRODUCE_CHANCE:
+                LargeAsteroid(self)
+
+        Game.update(self)
+
+'''I don't know how useful the asteroid code is to us, but it's written so that there isn't any code that's needlessly repeated. The asteroid and shootable classes handle almost everything, while the child classes just specify the color, shrapnel pieces, and type of shrapnel. IDK how feasible it would be, but we could try to implement something similar with either powerups or with the photons our players will shoot (assuming that some powerups will change how the photons act) '''
+
 class Asteroid(Shootable):
     WORTH     = 5
     MIN_SPEED = 0.1
     MAX_SPEED = 0.3
     SIZE      = 3.0
+
+    iFrames = 0 #Asteroids don't use these variables, but they're included to prevent crashes
+    shootDelay = 0
+    shotTimer = 0
 
     def __init__(self, position0, velocity0, world):
         Shootable.__init__(self,position0, velocity0, self.SIZE, world)
@@ -116,35 +326,6 @@ class ParentAsteroid(Asteroid):
         Asteroid.explode(self)
         self.world.number_of_asteroids -= 1
 
-class Ember(MovingBody):
-    '''Little sparks that come off when an asteroid is destroyed'''
-    INITIAL_SPEED = 2.0
-    SLOWDOWN      = 0.2
-    TOO_SLOW      = INITIAL_SPEED / 20.0
-
-    def __init__(self, position0, world):
-        velocity0 = Vector2D.random() * self.INITIAL_SPEED
-        MovingBody.__init__(self, position0, velocity0, world)
-
-    def color(self):
-        white_hot  = "#FFFFFF"
-        burning    = "#FF8080"
-        smoldering = "#808040"
-        speed = self.velocity.magnitude()
-        if speed / self.INITIAL_SPEED > 0.5:
-            return white_hot
-        if speed / self.INITIAL_SPEED > 0.25:
-            return burning
-        return smoldering
-
-    def steer(self):
-        return -self.velocity.direction() * self.SLOWDOWN
-
-    def update(self):
-        MovingBody.update(self)
-        if self.velocity.magnitude() < self.TOO_SLOW:
-            self.leave()
-
 class ShrapnelAsteroid(Asteroid):
     def __init__(self, position0, world):
         world.number_of_shrapnel += 1
@@ -183,163 +364,6 @@ class LargeAsteroid(ParentAsteroid):
 
     def color(self):
         return "#9890A0"
-
-'''I don't know how useful the asteroid code is to us, but it's written so that there isn't any code that's needlessly repeated. The asteroid and shootable classes handle almost everything, while the child classes just specify the color, shrapnel pieces, and type of shrapnel. IDK how feasible it would be, but we could try to implement something similar with either powerups or with the photons our players will shoot (assuming that some powerups will change how the photons act) '''
-
-class Photon(MovingBody):
-    '''Projectiles that the player shoots out'''
-    INITIAL_SPEED = 2.0 * SmallAsteroid.MAX_SPEED
-    LIFETIME      = 40
-
-    def __init__(self,source,world):
-        self.age  = 0
-        v0 = source.velocity + (source.get_heading() * self.INITIAL_SPEED)
-        MovingBody.__init__(self, source.position, v0, world)
-
-    def color(self):
-        return "#8080FF"
-
-    def update(self):
-        MovingBody.update(self)
-        self.age += 1
-        if self.age >= self.LIFETIME:
-            self.leave()
-        else:
-            targets = [a for a in self.world.agents if isinstance(a,Shootable)]
-            for t in targets:
-                if t.is_hit_by(self):
-                    t.explode()
-                    self.leave()
-                    return
-
-class Ship(MovingBody):
-    TURNS_IN_360   = 24
-    IMPULSE_FRAMES = 4
-    ACCELERATION   = 0.05
-    MAX_SPEED      = 2.0
-
-    START_X   = 5
-    START_Y   = 5
-
-    def __init__(self,world, player_one=True):
-        self.player_one = player_one
-        xoffset = -self.START_X if player_one else  self.START_X
-        yoffset =  self.START_Y if player_one else -self.START_Y
-        position0    = Point2D(xoffset, yoffset)
-        velocity0    = Vector2D(0.0,0.0)
-        MovingBody.__init__(self,position0,velocity0,world)
-        self.speed   = 0.0
-        self.angle   = 90.0
-        self.impulse = 0
-
-    def color(self):
-        if self.player_one:
-            return "#f74830" #Player one is red
-        else:
-            return "#3090f7" #Player two is blue
-
-    def get_heading(self):
-        angle = self.angle * math.pi / 180.0
-        return Vector2D(math.cos(angle), math.sin(angle))
-
-    def turn_left(self):
-        self.angle += 360.0 / self.TURNS_IN_360
-
-    def turn_right(self):
-        self.angle -= 360.0 / self.TURNS_IN_360
-
-    def speed_up(self):
-        self.impulse = self.IMPULSE_FRAMES
-
-    def shoot(self):
-        '''If need be, we can add self.player_one to the variables given to photon and then add code to it so that photons don't hit the player that created them '''
-        Photon(self, self.world)
-
-    def shape(self):
-        h  = self.get_heading()
-        hp = h.perp()
-        p1 = self.position + h
-        p2 = self.position + hp * 0.5
-        p3 = self.position - hp * 0.5
-        return [p1,p2,p3]
-
-    def steer(self):
-        if self.impulse > 0:
-            self.impulse -= 1
-            return self.get_heading() * self.ACCELERATION
-        else:
-            return Vector2D(0.0,0.0)
-
-    def trim_physics(self):
-        MovingBody.trim_physics(self)
-        m = self.velocity.magnitude()
-        if m > self.MAX_SPEED:
-            self.velocity = self.velocity * (self.MAX_SPEED / m)
-            self.impulse = 0
-
-class PlayAsteroids(Game):
-
-    DELAY_START      = 150
-    MAX_ASTEROIDS    = 6
-    INTRODUCE_CHANCE = 0.01
-
-    def __init__(self):
-        Game.__init__(self,"ASTEROIDS!!!",60.0,45.0,800,600,topology='wrapped')
-
-        self.number_of_asteroids = 0
-        self.number_of_shrapnel = 0
-        self.level = 1
-        self.score = 0
-
-        self.before_start_ticks = self.DELAY_START
-        self.started = False
-
-        self.ship_one = Ship(self, player_one=True)
-        self.ship_two = Ship(self, player_one=False)
-
-    def max_asteroids(self):
-        return min(2 + self.level,self.MAX_ASTEROIDS)
-
-    def handle_keypress(self,event):
-        Game.handle_keypress(self,event)
-
-        '''Player One controls: wasd + c to shoot '''
-        if event.char == 'w':
-            self.ship_one.speed_up()
-        if event.char == 'a':
-            self.ship_one.turn_left()
-        elif event.char == 'd':
-            self.ship_one.turn_right()
-        if event.char == 'c':
-            self.ship_one.shoot()
-
-        '''Player Two controls: pl;' + , to shoot '''
-        if event.char == 'p':
-            self.ship_two.speed_up()
-        if event.char == 'l':
-            self.ship_two.turn_left()
-        elif event.char == '\'':
-            self.ship_two.turn_right()
-        if event.char == ',':
-            self.ship_two.shoot()
-
-    def update(self):
-
-        # Are we waiting to toss asteroids out?
-        if self.before_start_ticks > 0:
-            self.before_start_ticks -= 1
-        else:
-            self.started = True
-
-        # Should we toss a new asteroid out?
-        if self.started:
-            tense = (self.number_of_asteroids >= self.max_asteroids())
-            tense = tense or (self.number_of_shrapnel >= 2*self.level)
-            if not tense and random.random() < self.INTRODUCE_CHANCE:
-                LargeAsteroid(self)
-
-        Game.update(self)
-
 
 print("Player one (red): Press a and d to turn, w to create thrust, and c to shoot. \nPlayer two (blue): Press l and \' to turn, p to create thrust, and COMMA to shoot. \nPress q to quit.")
 game = PlayAsteroids()
